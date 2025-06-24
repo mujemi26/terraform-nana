372e438722e223cae396b7e8d4b265cd096633c9
mujimmy <mohamed.kingzzz@gmail.com>
2025-06-24 17:58:56 +0300

    Add AWS security group resource for myapp with ingress and egress rules

--- main.tf
+++ main.tf
@@ -61,3 +61,34 @@ resource "aws_default_route_table" "myapp-default-rt" {
         Name = "${var.env_prefix}-rtb"
     }
 }
+
+resource "aws_security_group" "myapp-sg" {
+    name = "myapp-sg"
+    description = "Security group for myapp"
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    tags = {
+        Name = "${var.env_prefix}-sg"
+    }
+}
+
