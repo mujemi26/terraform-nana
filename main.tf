5b85c0bf3a802ee335881e21b7d5c5950f8cfa73
mujimmy <mohamed.kingzzz@gmail.com>
2025-06-24 11:30:23 +0300

    Refactor AWS infrastructure setup by introducing variables for VPC and subnet configurations, and updating resource definitions for a more modular approach. Replace hardcoded values with variable references for better flexibility and maintainability.

--- main.tf
+++ main.tf
@@ -1,28 +1,63 @@
 provider "aws" {
   region = "us-east-1"
 }
+#variables
 
-resource "aws_vpc" "dev-team" {
-  cidr_block = "192.168.0.0/16"
-  tags = {
-    Name = "dev-team"
-  }
+variable "vpc_cidr_block" {}
+
+variable "subnet_cidr_block" {}
+
+variable "availability_zone" {}
+
+variable "env_prefix" {}
+
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_block
+    tags = {
+        Name = "${var.env_prefix}-vpc"
+    }
 }
 
-resource "aws_subnet" "dev-team-subnet1" {
-  vpc_id = aws_vpc.dev-team.id
-  cidr_block = "192.168.1.0/24"
-  availability_zone = "us-east-1a"
-  tags = {
-    Name = "dev-team-subnet1"
-  }
+resource "aws_subnet" "myapp-subnet-1" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.availability_zone
+    tags = {
+        Name = "${var.env_prefix}-subnet-1"
+    }
 }
 
-resource "aws_subnet" "dev-team-subnet2" {
-  vpc_id = aws_vpc.dev-team.id
-  cidr_block = "192.168.2.0/24"
-  availability_zone = "us-east-1b"
-  tags = {
-    Name = "dev-team-subnet2"
-  }
+
+resource "aws_internet_gateway" "myapp-igw" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    tags = {
+        Name = "${var.env_prefix}-igw"
+    }
+}
+
+resource "aws_route_table" "myapp-route-table" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+    tags = {
+        Name = "${var.env_prefix}-route-table"
+    }
+}
+
+resource "aws_route_table_association" "myapp-rt-association" {
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    route_table_id = aws_route_table.myapp-route-table.id
+}
+
+resource "aws_default_route_table" "myapp-default-rt" {
+    default_route_table_id = aws_route_table.myapp-route-table.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+    tags = {
+        Name = "${var.env_prefix}-rtb"
+    }
 }
